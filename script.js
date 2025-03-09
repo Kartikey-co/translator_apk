3398d0b42ce3a87f451a4edc6ceddd317bc38f54
Kartikey-co <146407899+Kartikey-co@users.noreply.github.com>
2025-03-09 15:02:21 +0530

    add.script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//handle navbar slider
+const navbar = document.querySelectorAll(".nav-link");
+const nav_collapse = document.querySelector(".navbar-collapse ");
+
+navbar.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        nav_collapse.classList.remove("show");
+    });
+});
+
+
+// navbar icon
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+
